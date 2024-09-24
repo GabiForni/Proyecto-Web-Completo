25fd4724caaf3cda2b32232e2842fbab9d1844cd
Gabriel <zurditoaic@gmail.com>
2024-09-24 18:07:06 -0300

    CREANDO NUEVAS APPS Y BBDD

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ProyectoWebApp',
+    'Servicios',
+    'Contacto',
+    'Tienda',
+    'Blog',
 ]
 
 MIDDLEWARE = [
