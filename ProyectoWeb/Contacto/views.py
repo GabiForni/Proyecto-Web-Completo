62e5650f3d980c9cbfdea4fef7aa12726264c6a4
Gabriel <zurditoaic@gmail.com>
2024-10-02 18:57:38 -0300

    capturando info del form y validando datos

--- ProyectoWeb/Contacto/views.py
+++ ProyectoWeb/Contacto/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import Formulario_contacto
 # Create your views here.
 
 def contacto(request):
     formulario_contacto = Formulario_contacto()
+    if request.method == 'POST':
+        formulario_contacto = Formulario_contacto(data = request.POST)
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get('nombre')
+            mail = request.POST.get('email')
+            contenido = request.POST.get('contenido')
+
+            return redirect('/contacto/?valido')
     return render(request, 'contacto/contacto.html', {'mi_formulario': formulario_contacto})
 
