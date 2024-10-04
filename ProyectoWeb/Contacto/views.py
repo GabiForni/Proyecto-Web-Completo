043e109434c88378ad99dc84304ce8b46a19e566
Gabriel <zurditoaic@gmail.com>
2024-10-03 22:29:10 -0300

    configurando views.py con EmailMessage

--- ProyectoWeb/Contacto/views.py
+++ ProyectoWeb/Contacto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import Formulario_contacto
+from django.core.mail import EmailMessage
+from django.conf import settings
 # Create your views here.
 
 def contacto(request):
@@ -8,9 +10,16 @@ def contacto(request):
         formulario_contacto = Formulario_contacto(data = request.POST)
         if formulario_contacto.is_valid():
             nombre = request.POST.get('nombre')
-            mail = request.POST.get('email')
+            email = request.POST.get('email')
             contenido = request.POST.get('contenido')
 
-            return redirect('/contacto/?valido')
+            email = EmailMessage('Mensaje desde App Django', 
+            'El usuario con nombre {} con la direccion {} escribe lo siguiente:\n\n {}'
+            .format(nombre, email, contenido), '',[settings.EMAIL_HOST_USER], reply_to=[email])
+            try:
+                email.send()
+                return redirect('/contacto/?valido')
+            except:
+                return redirect('/contacto/?no_valido')
     return render(request, 'contacto/contacto.html', {'mi_formulario': formulario_contacto})
 
